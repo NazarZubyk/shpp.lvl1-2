1d2a920dd4da24f0e95e28d66ceb7838654579a9
Nazar Zubyk <kukurudza.v.tanke@gmail.com>
2023-06-24 00:03:23 +0300

    adds getter and setter

--- src/task1.1.js
+++ src/task1.1.js
@@ -16,18 +16,82 @@ function Product(ID,name,description,price,brand,size,activeSize,quantity,date,r
     this.getProductID = function(){
         return this.ID;
     }
-    function setProductId(newId){
+    this.setProductId = function(newId){
         this.ID = newId;
     }
-    this.getReviewById = function(){
-        let line = "";
-        for(let rev of reviews){
-            line = line + rev.ID + " "
-        }
-        return line;
+    
+    this.getProductName = function(){
+        return this.name;
     }
-    function setReviewById(newId){
-        this.reviews.ID = newId;
+    this.setProductName = function(newName){
+        this.name = newName
+    }
+    
+    this.getProductDescription = function(){
+        return this.description;
+    }
+    this.setProductDescription = function(newDescription){
+        this.description = newDescription;
+    }
+    
+    this.getProguctPrice = function(){
+        return this.price;
+    }
+    this.setProductPrice = function(newPrice){
+        this.price = newPrice;
+    }
+
+    this.getProductBrand = function(){
+        return this.brand;
+    }
+    this.setProductBrand = function (newBrand){
+        this.brand = newBrand;
+    }
+
+    this.getProductSize = function(){
+        return this.size;
+    }
+    this.setProductSize = function(newSize){
+        this.size = newSize;
+    }
+
+    this.getProductActiveSize = function(){
+        return this.activeSize;
+    }
+    this.setProductActiveSize = function(newActiveSize){
+        this.activeSize = newActiveSize;
+    }
+
+    this.getProductQuantity = function(){
+        return this.quantity;
+    }
+    this.setProductQuantity = function(newQuantity){
+        this.quantity = newQuantity;
+    }
+
+    this.getProductDate = function(){
+        return this.date;
+    }
+    this.setProductDate = function(newDate){
+        this.date = newDate;
+    }
+
+    this.getProductReviews = function(){
+        return this.reviews;
+    }
+    this.setProductReviews = function(newReviews){
+        this.reviews = newReviews;
+    }
+    
+    this.getProductImages = function(){
+        return this.images;
+    }
+    this.setProductImages = function(newImages){
+        this.images = newImages;
+    }
+
+    this.getReviewByID = function(){
+
     }
 }
 
@@ -37,13 +101,44 @@ function review(ID,author,date,comment,rating){
     this.date = date;
     this.comment = comment;
     this.rating = rating;
+
+    this.getID = function(){
+        return this.ID;
+    }
+    this.setID = function(newRevID){
+        this.ID = newRevID;
+    }
+    this.getAuthor = function(){
+        return this.author;
+    }
+    this.setAutor = function(newAuthor){
+        this.author = newAuthor;
+    }
+    this.getDate = function(){
+        return this.date;
+    }
+    this.setDate = function(newRevDate){
+        this.date = newRevDate;
+    }
+    this.getComment = function(){
+        return this.comment
+    }
+    this.setComment = function(newComment){
+        this.comment = newComment;
+    }
+    this.getRating = function(){
+        return this.rating; 
+    }
+    this.setRating = function(newRating){
+        this.rating = newRating;
+    }
 }
 
-let reviews;
+
 let review1 = new review('10',"Biba",new Date,"not good",{service : 10, price : 8, value : 7, quality : 6});
 let review2 = new review('20',"Biba2",new Date,"good",{service : 9, price : 9, value : 9, quality : 9});
 
-reviews = [review1,review2];
+ let reviews = [review1,review2];
 
 let product1 = new Product("1","apple","red",10,"wood",['XS', 'S', 'M', 'L', 'XL', 'XXL'],'S',23,new Date(),reviews,["image1","image2","image3"]);
 
