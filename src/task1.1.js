68534a64e03a4f635f9e3b453c026121229a4bf2
Nazar Zubyk <kukurudza.v.tanke@gmail.com>
2023-06-29 21:28:39 +0300

    Adds new methods in Product, comments and tests.

--- src/task1.1.js
+++ src/task1.1.js
@@ -1,12 +1,25 @@
-console.log("imput");
 
-function Product(ID,name,description,price,brand,size,activeSize,quantity,date,reviews,images){
+/**
+ * 
+ * @param {String} ID 
+ * @param {String} name 
+ * @param {String} description 
+ * @param {Float} price 
+ * @param {String} brand 
+ * @param {Array of string} sizes 
+ * @param {String} activeSize 
+ * @param {Integer} quantity 
+ * @param {Date} date 
+ * @param {Array of odject} reviews 
+ * @param {Array<String>} images 
+ */
+function Product(ID,name,description,price,brand,sizes,activeSize,quantity,date,reviews,images){
     this.ID = ID;
     this.name = name;
     this.description = description;
     this.price = price;
     this.brand = brand;
-    this.size = size;
+    this.sizes = sizes;
     this.activeSize = activeSize;
     this.quantity = quantity;
     this.date = date;
@@ -48,11 +61,11 @@ function Product(ID,name,description,price,brand,size,activeSize,quantity,date,r
         this.brand = newBrand;
     }
 
-    this.getProductSize = function(){
-        return this.size;
+    this.getProductSizes = function(){
+        return this.sizes;
     }
-    this.setProductSize = function(newSize){
-        this.size = newSize;
+    this.setProductSizes = function(newSizes){
+        this.sizes = newSizes;
     }
 
     this.getProductActiveSize = function(){
@@ -90,11 +103,78 @@ function Product(ID,name,description,price,brand,size,activeSize,quantity,date,r
         this.images = newImages;
     }
 
-    this.getReviewByID = function(){
+    this.getReviewByID = function(IDofReview){
 
+        for(let review of this.reviews){
+            if(review.getID() === IDofReview){
+                return review;
+            }
+        }
+
+        return undefined;
+    }
+
+    this.getImage = function(index){
+        if(index == undefined){
+            return images[0];
+        }else{
+            return images[index];
+        }
+            
+        
+    }
+
+    this.addSize = function(size){
+        this.sizes.push(size);
+    }
+
+    this.deleteSize = function(key){
+        let startElements = this.sizes.slice(0,key);
+        let endElements = this.sizes.slice(++key,sizes.length);
+        this.sizes = startElements.concat(endElements);
+    }
+
+    this.addReview = function(newReview){
+        this.reviews.push(newReview);
+    }
+
+    this.deleteReview = function(IDreviewForDeleted){
+        for(let i = 0; i <= reviews.length;i++){
+            
+            if(IDreviewForDeleted === reviews[i].getID()){
+                let startElements = this.reviews.slice(0,i);
+                let endElements = this.reviews.slice(++i,reviews.length);
+                this.reviews = startElements.concat(endElements);
+                break;
+            } 
+        }
+    }
+
+    this.getAverageRating = function(){
+        let quantityOfMarks = 0;
+        let summOfMarks = 0;
+        for(let review of this.reviews){
+            let rating = review.getRating();
+            for(let mark in rating){
+                summOfMarks += rating[mark];
+                quantityOfMarks++;
+            }
+        }
+        console.log(summOfMarks);
+        console.log(quantityOfMarks);
+        return summOfMarks/quantityOfMarks;
     }
 }
-
+    
+    
+/**
+ * 
+ * @param {String} ID 
+ * @param {String} author 
+ * @param {Date} date 
+ * @param {string} comment 
+ * @param {Array<kay : value>} rating 
+ */
 function review(ID,author,date,comment,rating){
     this.ID = ID;
     this.author = author;
@@ -143,7 +223,43 @@ let review2 = new review('20',"Biba2",new Date,"good",{service : 9, price : 9, v
 let product1 = new Product("1","apple","red",10,"wood",['XS', 'S', 'M', 'L', 'XL', 'XXL'],'S',23,new Date(),reviews,["image1","image2","image3"]);
 
 
-console.log(product1.getProductID())
-console.log(product1.getReviewById())
 
-console.log("output")
+
+
+/* console.log(product1.getReviewByID('10'))
+console.log(product1.getReviewByID('20'))
+console.log(product1.getReviewByID('0')) */
+
+/* console.log(product1.getImage());
+console.log(product1.getImage(1));
+console.log(product1.getImage("fff")); */
+
+/* console.log(product1.getProductSizes());
+product1.addSize("newSize");
+console.log(product1.getProductSizes());
+product1.addSize();
+console.log(product1.getProductSizes()); */
+
+
+/* console.log(product1.getProductSizes());
+product1.deleteSize(1);
+console.log(product1.getProductSizes());
+product1.deleteSize(1);
+console.log(product1.getProductSizes());
+product1.deleteSize(100);
+console.log(product1.getProductSizes());
+product1.deleteSize(-100);
+console.log(product1.getProductSizes());
+product1.deleteSize(NaN);
+console.log(product1.getProductSizes()); */
+
+
+/* product1.addReview(new review('30',"Shurik",Date,"so so",{service : 8, price : 8, value : 8, quality : 8}));
+console.log(product1.getReviewByID('30')); */
+
+/* console.log(product1.reviews.length + ' start length');
+product1.deleteReview('10')
+console.log(product1.reviews.length + ' end length'); */
+
+/* console.log(product1.getAverageRating());
+ */
