bfd5abb32f90c097cd04482283a597e73b8a8468
Nazar Zubyk <kukurudza.v.tanke@gmail.com>
2023-07-21 19:52:43 +0300

    file for FP

---
+++ src/test.js
@@ -0,0 +1,22 @@
+
+/* function foo(){
+    return [1,2,3];
+}
+function bar(){
+    return{
+        x : 4,
+        y : 5,
+        z : 6
+    };
+}
+ */
+
+// Modifying each word
+let words = ["spray", "limit", "exuberant", "destruction", "elite", "present"];
+
+console.log(words)
+
+a = words.join(" - ")
+
+console.log(a)
+
